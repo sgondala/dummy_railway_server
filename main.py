6b14b5e4cf55fcde0a13b563e294aa8ab6972c70
Sashank Gondala <sgondala2@gmail.com>
2023-04-11 20:40:02 -0700

    Commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # A simple webserver to handle few get and post requests
 
 from flask import Flask, request
-import json
+import os
 
 app = Flask(__name__)
 
@@ -10,11 +10,13 @@ def get():
     return "GET request received"
 
 # Post method expects a json object
-@app.route('/post', methods=['POST'])
+@app.route('/process_email/', methods=['POST'])
 def post():
     data = request.get_json()
     print(data)
     return "POST request received"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run app on 0.0.0.0
+    # Pick port from osenv PORT or default to 5000
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
